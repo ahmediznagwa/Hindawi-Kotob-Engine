90a28e5555ca0276a87226747e109584774d3a19
Ahmed Shaarawy <ahmed.shaarawy@nagwa.com>
2023-05-09 09:44:57 +0300

    copying files to dist folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,4 +6,13 @@ const path = require("path");
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './public/packages', to: 'packages' },
+        { from: './public/fonts', to: 'fonts' },
+        { from: './public/index.html' },
+      ]
+    })
+  ]
 });
