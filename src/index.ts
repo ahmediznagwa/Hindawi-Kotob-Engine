0869375c67bd2f2b64bfbb6af2b061736a4d78aa
Ahmed Shaarawy <ahmed.shaarawy@nagwa.com>
2023-05-24 15:06:28 +0300

    removing on load

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ declare global {
 }
 export const hindawiReaders = (function () {
   const controller = new Controller();
-  window.addEventListener("load", () => {
-    controller.initWithChapters("packages/life_prison", json, css);
-  });
+  // window.addEventListener("load", () => {
+  //   controller.initWithChapters("packages/life_prison", json, css);
+  // });
   return {
     init: controller.initWithChapters.bind(controller),
   };
